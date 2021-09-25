298114e8b61688a9ab8590fc8680a6f9bea723d2
Ryan Wolf <ryantwolf1@gmail.com>
2021-09-24 20:37:16 -0500

    Make new changes match

--- model/tile.py
+++ model/tile.py
@@ -6,8 +6,13 @@ class Tile:
         self.type = TileType[tile_dict['type']]
         self.crop = Crop(tile_dict['crop'])
         self.p1_item = ItemType[tile_dict['p1_item']]
-        self.p1_item = ItemType[tile_dict['p2_item']]
+        self.p2_item = ItemType[tile_dict['p2_item']]
         self.turns_left_to_grow = tile_dict['turnsLeftToGrow']
         self.rain_totem_effect = tile_dict['rainTotemEffect']
         self.fertility_idol_effect = tile_dict['fertilityIdolEffect']
         self.scarecrow_effect = tile_dict['scarecrowEffect']
+
+        self.scarecrow_effect = tile_dict['scarecrowEffect']
+
+        def has_scarecrow_effect(self, player_id: int):
+            return self.scarecrow_effect >= 0 and self.scarecrow_effect + 1 != player_id
